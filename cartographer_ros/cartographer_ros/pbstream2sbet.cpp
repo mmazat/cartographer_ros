1cff40430cad9405ed5887cb5164afebfa10d2fb
mehdi <DDD>
2017-11-09 10:57:53 -0500

    removed

--- cartographer_ros/cartographer_ros/pbstream2sbet.cpp
+++ cartographer_ros/cartographer_ros/pbstream2sbet.cpp
@@ -1,9 +1,21 @@
 #include <ros/ros.h>
+#include "gflags/gflags.h"
+
+DEFINE_string(pose_graph_filename, "",
+              "Proto stream file containing the pose graph.");
+DEFINE_string(
+    urdf_filename, "",
+    "URDF file that contains static links for your sensor configuration.");
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "pbstream2SBET");
-  ros::NodeHandle nh;
+  FLAGS_alsologtostderr = true;
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, true);
 
-  ROS_INFO("Hello world!");
+  CHECK(!FLAGS_pose_graph_filename.empty())
+      << "-pose_graph_filename is missing.";
+  CHECK(!FLAGS_urdf_filename.empty())
+      << "-pose_graph_filename is missing.";
+
 }
